9c931adabf2973b5fa6bee32009d3434b33a94c1
AbigailCaniucura18 <abigailcanicura@gmail.com>
2025-05-27 12:57:59 -0400

    listo

--- 05 actividad/workbenchAbigailcaniucura.sql
+++ 05 actividad/workbenchAbigailcaniucura.sql
@@ -126,6 +126,175 @@ ALTER TABLE usuarios ADD Password VARCHAR(15) AFTER correo;
 
 ALTER TABLE tipo_usuarios ADD descripcion_tipo VARCHAR(200) AFTER nombre_tipo;
 
+ALTER TABLE productos
+ADD descripcion_producto VARCHAR(100) AFTER id_producto;
+
+ALTER TABLE detalle_ventas  -- Modificar tabla
+-- Agregar una restricción (FK)
+ADD CONSTRAINT fk_detalle_ventas_productos
+-- Añade referencia(FK)
+FOREIGN KEY (producto_id) REFERENCES
+productos(id_producto);
+
+
+INSERT INTO usuarios (
+    nombre_usuario, Password, correo, tipo_usuario_id, created_by, updated_by
+)
+VALUES (
+    'sistema',
+    '$2y$10$2pE', -- Contraseña encriptada (ejemplo realista con bcrypt)
+    'sistema@plataforma.cl',
+    NULL,
+    NULL,
+    NULL
+);
+
+INSERT INTO tipo_usuarios (
+    nombre_tipo,
+    descripcion_tipo,
+    created_by,
+    updated_by
+)
+VALUES (
+    'administrador',
+    'Accede a todas las funciones del sistema, incluida la administración de usuarios.',
+    1, -- creado por el usuario inicial
+    1  -- actualizado por el mismo
+),
+(
+    'vendedor',
+    'Gestiona las ventas y la atención al cliente, con acceso limitado a funciones administrativas.',
+    1, -- creado por el usuario inicial
+    1  -- actualizado por el mismo
+),
+(
+    'gerente',
+    'Supervisa la operación del sistema de ventas y analiza el rendimiento del equipo.',
+    1, -- creado por el usuario inicial
+    1  -- actualizado por el mismo
+),
+(
+    'cliente',
+    'Accede a su perfil personal, historial de compras y estado de sus pedidos.',
+    1, -- creado por el usuario inicial
+    1  -- actualizado por el mismo
+);
+
+INSERT INTO usuarios (
+    nombre_usuario, Password, correo, tipo_usuario_id, created_by, updated_by
+)
+VALUES (
+    'abigail.caniucura',
+    'lalala1234', -- bcrypt hasheado
+    'abigailcaniucura@gmail.com',
+    1,  -- tipo: Administrador
+    1,   1  -- creado por el usuario "sistema"
+
+),
+(
+    'yetzibel.gonzales',
+    'lalu4321', -- bcrypt hasheado
+    'yetzibelgonzales@gmail.com',
+    2,  -- tipo: Administrador
+    1,   1  -- creado por el usuario "sistema"
+
+),
+(
+    'mariangel.pirona',
+    'jijijaja654', -- bcrypt hasheado
+    'mariangelpirona@gmail.com',
+    4,  -- tipo: Administrador
+    1,   1  -- creado por el usuario "sistema"
+
+),
+(
+    'ignacio.garrido',
+    'tukituki', -- bcrypt hasheado
+    'ignaciogarrido@gmail.com',
+    3,  -- tipo: Administrador
+    1,   1  -- creado por el usuario "sistema"
+
+),
+(
+    'benjaminrios',
+    'weko', -- bcrypt hasheado
+    'benjaminrios@gmail.com',
+    4,  -- tipo: Administrador
+    1,   1  -- creado por el usuario "sistema"
+
+);
+
+
+INSERT INTO productos (
+    nombre_producto, descripcion_producto, precio, stock, created_by, updated_by
+)
+VALUES (
+	'Utek Fuente de poder micro atx 500w', -- nombre del producto
+    'Fuente de poder de 500W para gabinetes micro ATX, ideal para equipos de bajo consumo.',
+    21.990, -- precio
+    100, -- stock
+    3,  
+    3
+
+),
+(
+    'Unidad De Reparación De Computadora Portátil Intel Core 4 I7 3630qm', -- nombre del producto
+    'Servicio de reparación con procesador Intel Core i7 de tercera generación para notebooks.',
+    72.632, -- precio
+    90, -- stock
+    3,  
+    3
+
+),
+(
+    'Placa Madre Original Dell Inspiron 3542 3552 Fabricante De Equipos',
+    'Placa base OEM compatible con laptops Dell Inspiron, ideal para reemplazos originales.',
+    28.187, -- precio
+    200, -- stock
+    3,  
+    3
+
+),
+(
+    'Monitor Gamer 23.8'' 24F1P FHD 1K 180HZ 1MS FreeSync HDMI',
+    'Monitor de alto rendimiento para gaming con 180Hz de refresco y 1ms de respuesta.',
+    99.990, -- precio
+    250, -- stock
+    3,  
+    3
+
+),
+(
+    'Memoria RAM DDR4 3200MT/s Crucial PRO CP2K16G4DFRA32A',
+    'Kit de RAM DDR4 Crucial PRO 3200MT/s, ideal para alto rendimiento en multitarea y juegos.',
+    64.660, -- precio
+    900, -- stock
+    3,  
+    3
+
+);
+
+
+select*from productos;
+select*from tipo_usuarios;
+select*from usuarios;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
